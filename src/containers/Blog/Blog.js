0236a10de156b3a96b14c4c6f75510eba6ed0a32
Harrish Suhumar <harrish.s@hotmail.ca>
2020-01-06 14:40:04 -0500

    fetch post and display when clicking on post button

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,24 +8,43 @@ import "./Blog.css";
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedPostId: null
   };
 
   componentDidMount() {
     axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
-      this.setState({ posts: response.data });
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map(post => {
+        return {
+          ...post,
+          author: "Max"
+        };
+      });
+      this.setState({ posts: updatedPosts });
     });
   }
 
+  postSelectedHandler = id => {
+    this.setState({ selectedPostId: id });
+  };
+
   render() {
     const posts = this.state.posts.map(post => {
-      return <Post title={post.title} key={post.id} />;
+      return (
+        <Post
+          title={post.title}
+          key={post.id}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      );
     });
     return (
       <div>
         <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
